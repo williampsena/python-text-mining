095818bd4e9581a36ba1c25cad3f85eda99041af
william.sena <william.sena@skyhub.com.br>
2019-08-14 09:42:24 -0300

    First sample with stop words

---
+++ 01_stop_words.py
@@ -0,0 +1,35 @@
+import pprint
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+pp = pprint.PrettyPrinter(width=41, compact=True)
+
+vegeta_quotes = [
+    ('voce nao e derrotado quando perde, mais sim quando você desiste.', 'confiante'),
+    ('O melhor guerreiro nao e aquele que sempre ganha, mas o que mantem o seu orgulho mesmo na derrota', 'orgulhoso'),
+    ('Enquanto o inimigo estiver na minha frente, eu lutarei.', 'confiante'),
+    ('Eu sou calmo e tenho o coracao puro... mas e pura maldade.', 'sincero'),
+    ('Meu coração e puro... pura maldade!', 'sincero'),
+    ('Verme insolente nao entre na frente.', 'bravo'),
+    ('O miseravel e um genio.', 'feliz')
+]
+
+stop_words = set(stopwords.words('portuguese') + [
+    ',',
+    'eu',
+    '!'
+])
+
+filtered = []
+
+for (quote, felling) in vegeta_quotes:
+    filtered.append(
+        (
+            [w for w in word_tokenize(quote.lower())
+             if not w in stop_words and w.isalpha()],
+            felling
+        )
+    )
+
+pp.pprint(filtered)
+# [ (['voce', 'nao', 'derrotado', 'perde', 'sim', 'desiste'], 'confiante'), ... ]
--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# python-text-mining
+## Python Text Mining - Examples
+## Activate environment
+
+First of all activate python virtual environment.
+
+```
+source .env/bin/activate
+```
+
+## Installation
+
+Install packages over pip
+
+```
+pip install -r requirements.txt
+```
+
+### NLTK Packages
+
+NTLK requires some extra data.
+
+```
+python -m nltk.downloader popular
+```
+
+### Run samples
+
+NTLK requires some extra data.
+
+```
+python 01_stop_words.py
+```
